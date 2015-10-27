d2f42336ce7f0492f4118cc42f0c7dd483538f91
Jon Sherrard <jon@andthats.it>
2015-10-27 15:18:50 +0000

    mit've buggered it

---
+++ plugin.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Agreable Pugpig Plugin
+ * Plugin URI:        https://github.com/shortlist-digital/agreable-plugin-plugin
+ * Description:       Packages posts into PugPig compatible issues
+ * Version:           0.1.0
+ * Author:            Shortlist Media
+ * Author URI:        http://shortlistmedia.co.uk
+ * License:           MIT
+ */
+
+if(file_exists(__DIR__ . '/vendor/autoload.php')){
+  require_once __DIR__ . '/vendor/autoload.php';
+} else if(file_exists(__DIR__ . '/../../../../vendor/getherbert/')){
+  require_once __DIR__ . '/../../../../vendor/autoload.php';
+} else {
+  throw new Exception('Something went badly wrong');
+}
+
+if(file_exists(__DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php')){
+  require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
+} else if(file_exists(__DIR__ . '/../../../../vendor/getherbert/framework/bootstrap/autoload.php')){
+  require_once __DIR__ . '/../../../../vendor/getherbert/framework/bootstrap/autoload.php';
+} else {
+  throw new Exception('Something went badly wrong');
+}
