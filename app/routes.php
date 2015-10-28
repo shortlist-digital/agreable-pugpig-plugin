fd2e1016e13b84632617f84b6a6b4396542f0959
Jon Sherrard <jon@andthats.it>
2015-10-28 16:34:59 +0000

    Starting editions feed work via api

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,10 @@
-<?php namespace MyPlugin;
+<?php namespace AgreablePugpigPlugin;
 
 /** @var \Herbert\Framework\Router $router */
+
+$router->get([
+  'as'   => 'editionFeed',
+  'uri'  => '/editionfeed/{id}/pugpig_atom_contents.manifest',
+  'uses' => __NAMESPACE__ . '\Controllers\EditionsFeedController@feed'
+]);
+
