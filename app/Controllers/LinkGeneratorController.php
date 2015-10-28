ca6b8fb893ffc4ec0859e944f1d62274facb5d9d
Jon Sherrard <jon@andthats.it>
2015-10-28 15:31:30 +0000

    link generator and path fixes

--- app/Controllers/LinkGeneratorController.php
+++ app/Controllers/LinkGeneratorController.php
@@ -9,13 +9,13 @@ class LinkGeneratorController {
   }
 
   public function edition_atom_url($edition_id) {
-    return get_bloginfo('url') . "/editionfeed/". $edition_id;
+    return get_bloginfo('url') . "/editionfeed/". $edition_id.$this->manifest_file_name;;
   }
 
   public function edition_preview_url($edition_id) {
     $base = Helper::pluginDirectory() . "reader/reader.html?atom=";
-    $edition_atom_feed = $this->edition_atom_url($edition_id);
-    return urlencode($base.$edition_atom_feed.$this->manifest_file_name);
+    $edition_atom_feed = urlencode($this->edition_atom_url($edition_id));
+    return $base.$edition_atom_feed;
   }
 
 }
--- app/hooks.php
+++ app/hooks.php
@@ -4,19 +4,26 @@ namespace AgreablePugpigPlugin\Hooks;
 use AgreablePugpigPlugin\Controllers\EditionsAdminController;
 use AgreablePugpigPlugin\Helper;
 
-function add_paths($paths){
-  // Get views specified in herbert.
-  $namespaces = Helper::get('views');
-  foreach ($namespaces as $namespace => $views){
-    foreach ((array) $views as $view){
-      // Add to timber $paths array.
-      array_unshift($paths, $view);
-    }
+class LoaderHelper {
+
+  public function init() {
+    \add_filter('timber/loader/paths', array($this, 'add_paths'), 10);
   }
-  return $paths;
+
+  function add_paths($paths){
+    // Get views specified in herbert.
+    $namespaces = Helper::get('views');
+    foreach ($namespaces as $namespace => $views){
+      foreach ((array) $views as $view){
+        // Add to timber $paths array.
+        array_unshift($paths, $view);
+      }
+    }
+    return $paths;
+  }
 }
 
-\add_filter('timber/loader/paths','add_paths', 10);
+(new LoaderHelper)->init();
 
 
 (new EditionsAdminController)->init();
