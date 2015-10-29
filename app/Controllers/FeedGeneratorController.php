ea8850ef171dde34416722bf2271308e229ccb1d
Jon Sherrard <jon@andthats.it>
2015-10-29 12:31:59 +0000

    edition feed working

--- app/Controllers/FeedGeneratorController.php
+++ app/Controllers/FeedGeneratorController.php
@@ -20,9 +20,22 @@ class FeedGeneratorController {
     return $new_time;
   }
 
-  function edition_feed($feed_data) {
+  function convert_permalink_to_manifest($permalink) {
+    $base = get_bloginfo('url');
+    $stripped = str_replace($base, "", $permalink);
+    return "../..".$stripped."pugpig.manifest";
+  }
+
+  function convert_permalink_to_index($permalink) {
+    $base = get_bloginfo('url');
+    $stripped = str_replace($base, "", $permalink);
+    return "../..".$stripped."pugpig_index.html";
+  }
+
+  function edition_feed($feed_data, $post_data) {
     return view('@AgreablePugpigPlugin/edition_feed.twig', array(
-      'feed' => $feed_data
+      'feed' => $feed_data,
+      'posts' => $post_data
     ))->getBody();
   }
 
